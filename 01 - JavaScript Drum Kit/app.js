1e9aeeede3944c996776dfc21b07f74dc2cb763f
Amy Moretti <amyemoretti@gmail.com>
2018-11-24 14:41:32 -0600

    day one complete

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,3 +1,21 @@
-window.addEventListener('keydown', function(e){
-    console.log(keycode);
-});
+
+function playSound() {   
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    if (!audio) return; // Stops function
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+};
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return; //skips if its not a transform
+    this.classList.remove("playing");
+};
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+
+window.addEventListener("keydown", playSound);
